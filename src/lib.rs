d08b276946338ffb61bd64b36e2f308d48db4976
Iwancof <rock.iwanof@gmail.com>
2024-03-02 19:42:55 +0900

    add tests for torus

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,12 @@
+#[cfg(test)]
+#[macro_use]
+extern crate approx;
+
 /// PolynomialRing<N, T> is a polynomial ring of quotient ring.
+#[derive(Clone, Copy, PartialEq, Eq)]
 pub struct PolynomialRing<const N: usize, T> {
     coefficients: [T; N],
+    // coefficients[i] is the coefficient of x^i
 }
 
 impl<const N: usize, T> PolynomialRing<N, T> {
@@ -62,6 +68,48 @@ where
     }
 }
 
+impl<const N: usize, T> std::fmt::Debug for PolynomialRing<N, T>
+where
+    T: std::fmt::Debug,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "PolynomialRing({:?})", self.coefficients)
+    }
+}
+
+impl<const N: usize, T> std::fmt::Display for PolynomialRing<N, T>
+where
+    T: std::fmt::Display + num_traits::identities::Zero,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut is_first = true;
+        let mut is_printed = false;
+        for (dim, cof) in self.coefficients.iter().enumerate().rev() {
+            if cof.is_zero() {
+                continue;
+            }
+            is_printed = true;
+
+            if !is_first {
+                write!(f, " + ")?;
+            }
+            is_first = false;
+            if dim == 0 {
+                write!(f, "{}", cof)?;
+            } else if dim == 1 {
+                write!(f, "{}x", cof)?;
+            } else {
+                write!(f, "{}x^{}", cof, dim)?;
+            }
+        }
+        if !is_printed {
+            write!(f, "0")?;
+        }
+
+        Ok(())
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -88,5 +136,18 @@ mod tests {
         let b = PolynomialRing::new([0, 1, 0]); // x
         let c = a * b; // x^3 + x % (x^2 + 1) = x - 1
         assert_eq!(c.coefficients, [-1, 1, 0]);
+    }
+
+    #[test]
+    fn test_torus() {
+        use fixed_torus::Torus;
+        let a = PolynomialRing::new([Torus::from(0.7), Torus::from(1.4)]);
+        let b = PolynomialRing::new([0, 1]);
+        let c = a * b;
+        // (0.4x + 0.7) * x = (0.4x^2 + 0.7x)
+        // (0.4x^2 + 0.7x) % (x^2 + 1) = (0.7x - 0.4) = 0.7x + 0.6
+
+        assert_relative_eq!(f64::from(c.coefficients[0]), 0.6, epsilon = 1e-6);
+        assert_relative_eq!(f64::from(c.coefficients[1]), 0.7, epsilon = 1e-6);
     }
 }
